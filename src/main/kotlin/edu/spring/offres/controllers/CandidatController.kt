0f5ce5dcb2e5a761dc358427cbe878b5c39b79ff
Nathannicolle <nathan-nicolle@orange.fr>
2023-02-17 12:02:11 +0100

    ajout du controller candidat

---
+++ src/main/kotlin/edu/spring/offres/controllers/CandidatController.kt
@@ -0,0 +1,21 @@
+package edu.spring.offres.controllers
+
+import edu.spring.offres.repositories.CandidatRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/candidat")
+class CandidatController {
+    @Autowired
+    lateinit var candidatRepository : CandidatRepository;
+
+    @RequestMapping(path = ["","index"])
+    fun indexListCandidat(model : ModelMap) : String
+    {
+        model["candidats"] = candidatRepository.findAll();
+        return "";
+    }
+}
