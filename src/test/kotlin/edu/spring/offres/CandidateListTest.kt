e2549266fce9e22ee20b4f95dfd8fe160e4d06af
Nathannicolle <nathan-nicolle@orange.fr>
2023-02-27 23:09:00 +0100

    ajout de dépendances pour faires les tests fonctionnels

--- src/test/kotlin/edu/spring/offres/CandidateListTest.kt
+++ src/test/kotlin/edu/spring/offres/CandidateListTest.kt
@@ -1,12 +1,22 @@
 package edu.spring.offres
 
+import edu.spring.offres.repositories.CandidatRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CandidateListTest {
+    /*private lateinit var driver: WebDriver
+    @BeforeEach
+    fun init() {
+        System.setProperty("webdriver.chrome.driver", "C:/ChromeDriver/chromedriver.exe")
+        driver = ChromeDriver()
+        driver.get("http://localhost:8080/ma-page-de-liste")
+    }*/
+
     @Test
     fun index() {
-        
+
     }
 }
