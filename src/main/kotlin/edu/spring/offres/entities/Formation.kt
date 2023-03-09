9d7a324885626d32b576d4eb2ccf0f8575882920
Nathannicolle <nathan-nicolle@orange.fr>
2023-03-09 10:42:34 +0100

    add formation repository + formation entity file

---
+++ src/main/kotlin/edu/spring/offres/entities/Formation.kt
@@ -0,0 +1,21 @@
+package edu.spring.offres.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+
+@Entity
+class Formation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    open var idFormation : Int? = null
+
+    @Column(length = 255, nullable = false)
+    open lateinit var libelle : String
+
+    @OneToMany
+    open var candidat =  mutableSetOf<Candidat>()
+}
