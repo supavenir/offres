7b086e5c5b8eccc63c062d9dbd7e167110b7ccc2
Nathannicolle <nathan-nicolle@orange.fr>
2023-03-28 18:34:09 +0200

    requête pour récupérer la formation d'un candidat
    créée dans le repository Candidat
    
    il s'agit d'une nativeQuery (requête spécifiée dans l'annotation @Query)

--- src/main/kotlin/edu/spring/offres/repositories/CandidatRepository.kt
+++ src/main/kotlin/edu/spring/offres/repositories/CandidatRepository.kt
@@ -1,9 +1,13 @@
 package edu.spring.offres.repositories
 
 import edu.spring.offres.entities.Candidat
+import edu.spring.offres.entities.Formation
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CandidatRepository: CrudRepository<Candidat, Int> {
+    @Query("SELECT formation.libelle FROM formation INNER JOIN formation_offres ON formation.id_formation= formation_offres.formations_id_formation INNER JOIN offre ON offre.id = formation_offres.offres_id INNER JOIN candidat_offre ON candidat_offre.offre_id = offre.id INNER JOIN candidat ON candidat.id_candidat = candidat_offre.candidat_id_candidat WHERE candidat.id_candidat = 1", nativeQuery = true)
+    fun findByFormation() : List<Formation>
 }
