897efb28a6a177e2c7aa256eb666ad7858313141
Nathannicolle <nathan-nicolle@orange.fr>
2023-03-29 14:36:24 +0200

    edited findByFormation query

--- src/main/kotlin/edu/spring/offres/controllers/CandidatController.kt
+++ src/main/kotlin/edu/spring/offres/controllers/CandidatController.kt
@@ -4,6 +4,7 @@ import edu.spring.offres.repositories.CandidatRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -16,6 +17,12 @@ class CandidatController {
     fun indexListCandidat(model : ModelMap) : String
     {
         model["candidats"] = candidatRepository.findAll();
+        return "/candidat/index";
+    }
+
+    @RequestMapping("/getByFormation/{id}")
+    fun listCandidatByFormation(model: ModelMap, @PathVariable id : Int) : String {
+        model["formation_candidat"] = candidatRepository.findByFormation(id)
         return "/candidat/index";
     }
 }
--- src/main/kotlin/edu/spring/offres/repositories/CandidatRepository.kt
+++ src/main/kotlin/edu/spring/offres/repositories/CandidatRepository.kt
@@ -4,10 +4,11 @@ import edu.spring.offres.entities.Candidat
 import edu.spring.offres.entities.Formation
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CandidatRepository: CrudRepository<Candidat, Int> {
-    @Query("SELECT formation.libelle FROM formation INNER JOIN formation_offres ON formation.id_formation= formation_offres.formations_id_formation INNER JOIN offre ON offre.id = formation_offres.offres_id INNER JOIN candidat_offre ON candidat_offre.offre_id = offre.id INNER JOIN candidat ON candidat.id_candidat = candidat_offre.candidat_id_candidat WHERE candidat.id_candidat = 1", nativeQuery = true)
-    fun findByFormation() : List<Formation>
+    @Query("SELECT * FROM \"formation\" INNER JOIN \"formation_offres\" ON \"formation\".\"id_formation\"= \"formation_offres\".\"formations_id_formation\" INNER JOIN \"offre\" ON \"offre\".\"id\" = \"formation_offres\".\"offres_id\" INNER JOIN \"candidat_offre\" ON \"candidat_offre\".\"offre_id\" = \"offre\".\"id\" INNER JOIN \"candidat\" ON \"candidat\".\"id_candidat\" = \"candidat_offre\".\"candidat_id_candidat\" WHERE \"candidat\".\"id_candidat\" = :idCandidat ", nativeQuery = true)
+    fun findByFormation(@Param("idCandidat") idCandidat : Int) : List<Formation>
 }
