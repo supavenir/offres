5e14fd1b4d5005c19ecdbc76cf8388600f24e922
Nathannicolle <nathan-nicolle@orange.fr>
2023-04-05 22:55:38 +0200

    switch entre l'affichage standard et par formation fonctionnel
    j'ai ajouté la possibilité de revenir à l'affichage standard
    
    et j'ai simplifier le remplacement des balises tbody dans la response

--- target/classes/static/js/candidat.js
+++ target/classes/static/js/candidat.js
@@ -5,12 +5,24 @@ window.addEventListener("load", () => {
          xhr.onreadystatechange = function () {
             // console.log(this)
             if(this.status === 200 && this.readyState === 4) {
-               let tbodyContent = this.responseText.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace("<tbody>", "").replace("</tbody>", "");
+               let tbodyContentByFormation = this.responseText.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace(/(<tbody>|<\/tbody>)/gmi, "");
+               // console.log(tbodyContent)
+               document.querySelector("tbody").innerHTML = tbodyContentByFormation;
+            }
+         };
+         xhr.open("GET", "/candidat/getByFormation", true);
+         xhr.send();
+      } else {
+         let xhr = new XMLHttpRequest();
+         xhr.onreadystatechange = function () {
+            // console.log(this)
+            if(this.status === 200 && this.readyState === 4) {
+               let tbodyContent = this.responseText.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace(/(<tbody>|<\/tbody>)/gmi, "");
                // console.log(tbodyContent)
                document.querySelector("tbody").innerHTML = tbodyContent;
             }
          };
-         xhr.open("GET", "/candidat/getByFormation", true);
+         xhr.open("GET", "/candidat/index", true);
          xhr.send();
       }
    });
