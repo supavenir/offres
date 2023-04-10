a68614fe3b9c7201963c2ae436cde8ed578d5660
Nathannicolle <nathan-nicolle@orange.fr>
2023-04-10 17:18:30 +0200

    ajout d'en-têtes à la requête
    pour passer au controller l'info que la requête souhaitée est partielle

--- target/classes/static/js/candidat.js
+++ target/classes/static/js/candidat.js
@@ -1,28 +1,38 @@
 window.addEventListener("load", () => {
-   // Switch du listage standard des candidats au listage par formation
+   // Partie 1 - Switch du listage standard des candidats au listage par formation
    document.querySelector("#toggl_formation").addEventListener("change", () => {
-      if(document.querySelector("#toggl_formation").checked) {
-         fetch("/candidat/getByFormation", { method: "GET" }).then(request => {
+       let headers = new Headers();
+       headers.append("x-partial_request", "true")
+       if(document.querySelector("#toggl_formation").checked) {
+         fetch("/candidat/getByFormation", {
+             method: "GET",
+             headers: headers
+         }).then(request => {
             request.text().then(requestBody => {
                if(request.ok === true) {
-                  let tbodyContentByFormation = requestBody.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace(/(<tbody>|<\/tbody>)/gmi, "");
-                  document.querySelector("tbody").innerHTML = tbodyContentByFormation;
+                  console.log(request)
+                  /*let tbodyContentByFormation = requestBody.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace(/(<tbody>|<\/tbody>)/gmi, "");
+                  document.querySelector("tbody").innerHTML = tbodyContentByFormation;*/
                }
             })
          });
       } else {
-         fetch("/candidat/index", { method: "GET" }).then(request => {
+         fetch("/candidat/index", {
+             method: "GET",
+             headers:headers
+         }).then(request => {
             request.text().then(requestBody => {
                if(request.ok === true) {
-                  let tbody = requestBody.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace(/(<tbody>|<\/tbody>)/gmi, "");
-                  document.querySelector("tbody").innerHTML = tbody;
+                  console.log(request)
+                  /*let tbody = requestBody.match(/<tbody>([\s\S]*?)<\/tbody>/i)[0].replace(/(<tbody>|<\/tbody>)/gmi, "");
+                  document.querySelector("tbody").innerHTML = tbody;*/
                }
             })
          });
       }
    });
 
-   // (Un/)Check all checkboxes on click on select all checkbox
+   // Partie 2 - (Un/)Check all checkboxes on click on select all checkbox
    document.querySelector("#select_all").addEventListener("change", () => {
       if (document.querySelector("#select_all").checked) {
          document.querySelectorAll(".checkbox_candidat").forEach((oneCheckbox) => {
